c653b2726851781d2c3f1faa8dd26460940c2302
Zhuqing <zengzhuqing@foxmail.com>
2015-08-07 22:04:18 +0800

    USE QXL and IDE in start_vm

--- start_vm.sh
+++ start_vm.sh
@@ -1,2 +1,2 @@
 QEMU=qemu-system-i386
-$QEMU -m 1024 -drive file=images/test.img,cache=writeback,if=virtio -net nic -net tap,script=no -vnc :1 -spice port=7001,disable-ticketing -enable-kvm -smp 1 -usbdevice tablet -soundhw all --full-screen
+sudo $QEMU -m 1024 -drive file=images/test.img,if=none,id=drive-ide0-0-0,format=raw,cache=none -device ide-hd,bus=ide.0,unit=0,drive=drive-ide0-0-0,id=ide0-0-0,bootindex=1 -drive if=none,id=drive-ide0-1-0,readonly=on,format=raw -net nic -net tap,script=no -vnc :1 -spice port=7001,disable-ticketing -enable-kvm -smp 1 -usbdevice tablet -soundhw all --full-screen -monitor stdio -vga qxl -global qxl-vga.ram_size=67108864 -global qxl-vga.vram_size=67108864 
