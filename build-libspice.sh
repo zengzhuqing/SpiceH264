2623e0384be5cb8bf2dd8d25afd6aadfb3ab9332
Zhuqing <zengzhuqing@foxmail.com>
2015-08-08 16:00:59 +0800

    Fix spice server build script

--- build-libspice.sh
+++ build-libspice.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd build-spice 
 ../spice-0.12.4/configure --prefix=$INSTALL_PATH --disable-celt051 --disable-silent-rules \
-		--disable-smartcard --enable-client
+		--disable-smartcard --enable-client 
 make
 sudo make install
+sudo cp /usr/lib/libspice-server.so.1.8.0 /usr/lib/i386-linux-gnu/libspice-server.so.1.8.0
+sudo rm /usr/lib/libspice-server*
