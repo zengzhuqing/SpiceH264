429e4f332ca83fef75313ea889f33db9f8308d6c
Zhuqing <zengzhuqing@foxmail.com>
2015-11-03 20:30:33 +0800

    FIX build-spice-protocol.sh to new source code version

--- build-spice-protocol.sh
+++ build-spice-protocol.sh
@@ -3,6 +3,6 @@ if [ ! -d build-spice-protocol ];then
     mkdir build-spice-protocol
 fi
 cd build-spice-protocol
-../spice-protocol-0.12.6/configure --prefix=$INSTALL_PATH
+../spice-protocol-0.12.10/configure --prefix=$INSTALL_PATH
 make
 make install
