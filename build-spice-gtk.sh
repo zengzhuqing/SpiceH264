939c55adca4f655d849083726af4b40a21c88e93
Zhuqing <zengzhuqing@foxmail.com>
2015-11-03 20:16:48 +0800

    FIX build-spice-gtk.sh

--- build-spice-gtk.sh
+++ build-spice-gtk.sh
@@ -5,7 +5,8 @@ if [ ! -d $BUILD_DIR ];then
 fi
 #Need to install perl-Text-CSV_XS.x86_64 before
 #Need to install Text::CSV perl module by `perl -MCPAN -e'install Text::CSV'`
+#Need to install python-six-1.3.0-4.el7.noarch 
 cd $BUILD_DIR 
-../spice-gtk3-0.22/configure --prefix=$INSTALL_PATH --bindir=$BIN_PATH --with-gtk=3.0 --enable-vala 
+../spice-gtk-0.30/configure --prefix=$INSTALL_PATH --bindir=$BIN_PATH --with-gtk=3.0 --enable-vala 
 make
 sudo make install
