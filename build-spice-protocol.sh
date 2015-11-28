2ead9e1725e56120fa4d54267123fc0efe512cf0
Zhuqing <zengzhuqing@foxmail.com>
2015-11-28 18:55:30 +0800

    FIX spice protocol header update error configure, make, make install does not update enums.h in build dir

--- build-spice-protocol.sh
+++ build-spice-protocol.sh
@@ -2,6 +2,7 @@
 if [ ! -d build-spice-protocol ];then
     mkdir build-spice-protocol
 fi
+# when change enums.h, please rm build-spice-protocol -rf to update
 cd build-spice-protocol
 ../spice-protocol-0.12.10/configure --prefix=$INSTALL_PATH
 make
