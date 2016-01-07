7c0786b925251568f10daf5dae917a0082144308
Zhuqing <zengzhuqing@foxmail.com>
2016-01-07 17:53:28 +0800

    Change build-qemu.sh to 2.3.0

--- build-qemu.sh
+++ build-qemu.sh
@@ -3,7 +3,7 @@ if [ ! -d build-qemu ];then
     mkdir build-qemu
 fi
 cd build-qemu 
-../qemu-1.5.3/configure --prefix=$INSTALL_PATH --bindir=$BIN_PATH --enable-kvm --enable-debug --enable-spice --enable-usb-redir --target-list="i386-softmmu x86_64-softmmu"
+../qemu-2.3.0/configure --prefix=$INSTALL_PATH --bindir=$BIN_PATH --enable-kvm --enable-debug --enable-spice --enable-usb-redir --target-list="i386-softmmu x86_64-softmmu"
 
 make
 make install
